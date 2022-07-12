314dc08d973cedc1c12d7e47d3cb218fe2835d7d
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-07-12 19:08:36 -0400

    final

--- public/app.js
+++ public/app.js
@@ -1,19 +1,44 @@
 // create map
-
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 // add openstreetmap tiles
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
 
 // create and main add geolocation marker
-
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // draw the 2nd arrondissement
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
 
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
 
 // create red pin marker
-
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
 
 // metro station markers
 
+var popup = L.popup();
 
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
 
